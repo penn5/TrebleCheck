a1a3c93a8dc7c35d4f520b19c15c7f485eed1b6e
Hackintosh 5 <hackintosh5@wildfyre.net>
2019-08-11 21:40:28 +0100

    Fix copyright

--- app/src/main/cpp/BinderDetector.h
+++ app/src/main/cpp/BinderDetector.h
@@ -1,6 +1,12 @@
-//
-// Created by penn on 11/08/19.
-//
+/*
+ * Sub-licenses:
+ *         https://github.com/google/material-design-icons/blob/master/LICENSE
+ *         https://github.com/Templarian/MaterialDesign/blob/master/LICENSE
+ *         https://android.googlesource.com/platform/prebuilts/maven_repo/android/+/master/NOTICE.txt
+ * This project:
+ *         Copyright (C) 2019 Penn Mackintosh
+ *         Licensed under https://www.gnu.org/licenses/gpl-3.0.en.html
+ */
 
 #ifndef TREBLECHECK_BINDERDETECTOR_H
 #define TREBLECHECK_BINDERDETECTOR_H
--- app/src/main/java/tk/hack5/treblecheck/ScrollingActivity.kt
+++ app/src/main/java/tk/hack5/treblecheck/ScrollingActivity.kt
@@ -1,3 +1,13 @@
+/*
+ * Sub-licenses:
+ *         https://github.com/google/material-design-icons/blob/master/LICENSE
+ *         https://github.com/Templarian/MaterialDesign/blob/master/LICENSE
+ *         https://android.googlesource.com/platform/prebuilts/maven_repo/android/+/master/NOTICE.txt
+ * This project:
+ *         Copyright (C) 2019 Penn Mackintosh
+ *         Licensed under https://www.gnu.org/licenses/gpl-3.0.en.html
+ */
+
 package tk.hack5.treblecheck
 
 import android.content.res.ColorStateList
