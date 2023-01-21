f4378b3e1238bdfaf39acbfb3b22138f96f5de7f
Hackintosh 5 <git@hack5.dev>
2023-01-21 18:40:23 +0000

    Update gitignore, adding Fastlane metadata to repo
    Images are not yet included

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:7.4.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-        classpath("com.github.penn5:poeditor-android:0.1.5")
+        classpath("com.github.penn5:poeditor-android:0.1.6")
         classpath("com.github.penn5:materialdesignicons-android:0.1.1")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
