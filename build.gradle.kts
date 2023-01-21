d7377fb1c5918875db648697dae96be4ecb82fa7
Hackintosh 5 <git@hack5.dev>
2023-01-21 18:09:54 +0000

    Fix newlines in drawables

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,8 +29,8 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:7.4.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-        classpath("com.github.penn5:poeditor-android:0.1.4")
-        classpath("com.github.penn5:materialdesignicons-android:0.1.0")
+        classpath("com.github.penn5:poeditor-android:0.1.5")
+        classpath("com.github.penn5:materialdesignicons-android:0.1.1")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
